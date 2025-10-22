42689742da9acda8f99f3b78bcc7c6296e3e2f4c
Sai <omotayodavid657@gmail.com>
2025-10-22 19:58:10 +0100

    Implementation of BankAccount class with deposit, withdraw, and display functionality

--- M1/Classes/Bank Account System/Account System/BankAccount.cs
+++ M1/Classes/Bank Account System/Account System/BankAccount.cs
@@ -2,5 +2,48 @@ namespace AccountSystem;
 
 public class BankAccount
 {
+    private decimal balance;
+    public string? AccountNumber { get; private set; }
+    public string? AccountHolder { get; set; }
+    public static int TotalTransactions { get; set; } = 0;
+
+    public BankAccount(string? accountNumber) : this(accountNumber,0m)
+    {
+    }
+
+    public BankAccount(string? accountNumber, decimal initialBalance)
+    {
+        AccountNumber = accountNumber;
+        balance = initialBalance;
+        AccountHolder = "";
+    }
+
+
+    public void Deposit(decimal amount)
+    {
+        balance += amount;
+        Console.WriteLine($"You have succesfully deposited £{amount}");
+        TotalTransactions++;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (balance < amount)
+        {
+            Console.WriteLine("Insufficient funds!");
+        }
+        else
+        {
+            balance -= amount;
+            Console.WriteLine("Withdrawal succesful!");
+            TotalTransactions++;
+        }
+    }
     
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Account Holder: {AccountHolder}");
+        Console.WriteLine($"Account Number: {AccountNumber}");
+        Console.WriteLine($"Current Balance: {balance}");
+    }
 }
--- M1/Classes/Bank Account System/Account System/Program.cs
+++ M1/Classes/Bank Account System/Account System/Program.cs
@@ -1,1 +1,32 @@
 using AccountSystem;
+Console.WriteLine("----------Bank Account System----------");
+
+Console.WriteLine("-----Creating New Account-----");
+Console.Write("Enter Account Number: ");
+string? accountNumber = Console.ReadLine();
+BankAccount account1 = new(accountNumber);
+Console.Write("Enter Account Name: ");
+account1.AccountHolder = Console.ReadLine();
+
+Console.WriteLine("-----Creating New Account-----");
+Console.Write("Enter Account Number: ");
+accountNumber = Console.ReadLine();
+Console.Write("Enter initial account balance: ");
+decimal initialBalance = decimal.TryParse(Console.ReadLine(), out decimal value) ? value : 0m;
+BankAccount account2 = new(accountNumber,initialBalance);
+Console.Write("Enter Account Name: ");
+account2.AccountHolder = Console.ReadLine();
+
+account1.Deposit(1000m);
+account1.Deposit(1000m);
+account1.Withdraw(3000m);
+
+account2.Deposit(1000m);
+account2.Deposit(1000m);
+account2.Withdraw(2000m);
+
+account1.DisplayInfo();
+account2.DisplayInfo();
+
+Console.WriteLine($"Total Transactions over all accounts: {BankAccount.TotalTransactions}");
+Console.ReadKey();
